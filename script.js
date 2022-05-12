3668cdc2e3775a45a2f5ef357e654b2bc8638bd2
Lakshyadeep Gogoi <Lakshyadeep19@gmail.com>
2022-05-13 00:11:08 +0530

    update portfolio
    i have update my portfolio

--- script.js
+++ script.js
@@ -1,75 +1,70 @@
 $(document).ready(function () {
   $(window).scroll(function () {
     if (this.scrollY > 20) {
-      $('.navbar').addClass("sticky");
+      $(".navbar").addClass("sticky");
     } else {
-      $('.navbar').removeClass("sticky");
+      $(".navbar").removeClass("sticky");
     }
-    if(this.scrollY > 500){
-      $('.scroll-up-btn').addClass("show");
-
-    }else{
-      $('.scroll-up-btn').removeClass("show");
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
     }
   });
 
-//!slide-up script
-$('.scroll-up-btn').click(function(){
-  $('html').animate({scrollTop: 0});
-});
-
-
+  //!slide-up script
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
 
   //!toggle menu/navbar script
-  $('.menu-btn').click(function(){
-    $('.navbar .menu').toggleClass("active");
-    $('.menu-btn i').toggleClass("active");
-      
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
   });
-
 
   //!typing animation script
 
   var typed = new Typed(".typing", {
     strings: ["Developer", "Blogger", "Designer", "Freelancer"],
     typedSpeed: 100,
-    backSpeed:60,
-    loop:true
-  })
+    backSpeed: 60,
+    loop: true,
+  });
 
   var typed = new Typed(".typing-2", {
     strings: ["Developer", "Blogger", "Designer", "Freelancer"],
     typedSpeed: 100,
-    backSpeed:60,
-    loop:true
-  })
+    backSpeed: 60,
+    loop: true,
+  });
 
   //owl carousel script
-  $('.carousel').owlCarousel({
+  $(".carousel").owlCarousel({
     margin: 20,
     loop: true,
     autoplayTimeOut: 2000,
     autoPlayHoverPause: true,
-    responsive:{
-      0:{
+    responsive: {
+      0: {
         items: 1,
-        nav:false
+        nav: false,
       },
-      600:{
+      600: {
         items: 2,
-        nav:false
+        nav: false,
       },
-      1000:{
-        items:3,
-        nav:false
-      }
-
-    }
-
-
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
   });
 });
 
+
+/*
+//! --- for inspect
 $(document).bind("contextmenu",function(e) {
   e.preventDefault();
   alert("Don't try to copy content")
@@ -79,4 +74,4 @@ $(document).bind("contextmenu",function(e) {
      if(e.which === 123){
         return false;
      }
- });
+ });*/
